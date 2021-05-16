4cca4ff35252cc099c2ef2236cb8823713579928
itdar <thesockerrr@gmail.com>
2021-05-17 01:34:13 +0900

    feat: Posts update 기능 및 반환 dto 추가

--- src/main/java/com/tistory/itdar/springboot/service/posts/PostsService.java
+++ src/main/java/com/tistory/itdar/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.tistory.itdar.springboot.service.posts;
 
+import com.tistory.itdar.springboot.domain.posts.Posts;
 import com.tistory.itdar.springboot.domain.posts.PostsRepository;
+import com.tistory.itdar.springboot.web.dto.PostsResponseDto;
 import com.tistory.itdar.springboot.web.dto.PostsSaveRequestDto;
+import com.tistory.itdar.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 
 }
