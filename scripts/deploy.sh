4c05979db2eee58048b53ed0e085b3b17c6c7a21
itdar <thesockerrr@gmail.com>
2021-07-11 19:10:40 +0900

    fix: 배포 후 자동 실행 안되는 버그 수정 중 커밋
    - 오타 재수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,14 +13,14 @@ echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl springboot-aws-web | grep jar | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+echo "현재 구동 중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-	echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-	echo "> kill -15 $CURRENT_PID"
-	kill -15 $CURRENT_PID
-	sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
@@ -41,9 +41,9 @@ echo "> $JAR_NAME 실행"
 # classpath 가 붙으면 jar 안에 있는 resources 디렉토리 기준으로 경로가 생성됨
 # application-oauth.properties 는 절대경로를 사용함. 외부에 파일이 있기 때문
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-        -Dspring.profiles.active=real \
-        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 # nohup 실행 시, Code deploy 는 무한대기 함
 # 이 이슈 해결을 위해 nohup.out 파일을 표준 입출력용으로 별도로 사용함
 # 이렇게 하지 않으면 nohup.out 파일이 생기지 않고 Code Deploy 로그에 표준 입출력이 출력됨
